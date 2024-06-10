e3f17b186dfdbce3c22cf563f819af6c4e50b36d
CIIT601PC29 <it@ciit.edu.ph>
2024-06-10 10:41:41 +0800

    sdasdasad

---
+++ Assets/GridGenerator.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridGenerator : MonoBehaviour
+{
+    public int width, height;
+    public GameObject tilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #region
+    public void GenerateGrid()
+    {
+        if (tilePrefab == null)
+        {
+            Debug.LogError("No tile");
+            return;
+        }
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Vector3 position = new Vector3(x, 0, y);
+                GameObject newTile = Instantiate(tilePrefab, position, Quaternion.identity);
+                newTile.transform.parent = transform;
+                newTile.tag = "Tile";
+            }
+        }
+    }
+    #endregion
+    public void ClearGrid()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        foreach(GameObject tile in tiles)
+        {
+            DestroyImmediate(tile);
+        }
+    }
+
+    public void AssignMaterial()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        Material material = Resources.Load<Material>("Tile");
+        foreach (GameObject tile in tiles)
+        {
+            tile.GetComponent<Renderer>().material = material;
+        }
+                
+        
+    }
+}
---
+++ Assets/GridGeneratorEditor.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GridGenerator))]
+public class GridGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        GridGenerator gridGenerator = (GridGenerator)target;
+        if (GUILayout.Button("Generate Grid"))
+        {
+            gridGenerator.GenerateGrid();
+        }
+        if (GUILayout.Button("Clear Grid"))
+        {
+            gridGenerator.ClearGrid();
+        }
+        if (GUILayout.Button("Assign Material"))
+        {
+            gridGenerator.AssignMaterial();
+        }
+    }
+
+    [MenuItem("Tools/Generate Grid")]
+    public static void GenerateGridMenu()
+    {
+        GridGenerator gridGenerator = FindAnyObjectByType<GridGenerator>();
+        if (gridGenerator != null)
+        {
+            gridGenerator.GenerateGrid();
+        }
+        else
+        {
+            Debug.LogError("No Grid Generator Found");
+        }
+    }
+    [MenuItem("Tools/Clear Grid")]
+    public static void ClearGridMenu()
+    {
+        GridGenerator gridGenerator = FindAnyObjectByType<GridGenerator>();
+        if (gridGenerator != null)
+        {
+            gridGenerator.ClearGrid();
+        }
+        else
+        {
+            Debug.LogError("No Grid Generator Found");
+        }
+    }
+
+    [MenuItem("Tools/Assign Material")]
+    public static void AssignMaterialMenu()
+    {
+        GridGenerator gridGenerator = FindAnyObjectByType<GridGenerator>();
+        if (gridGenerator != null)
+        {
+            gridGenerator.AssignMaterial();
+        }
+        else
+        {
+            Debug.LogError("No Grid Generator Found");
+        }
+    }
+}
